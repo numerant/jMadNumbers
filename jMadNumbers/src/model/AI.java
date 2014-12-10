51764f3f3e11d008c71a481ca9755f45108f5b7e
Jakub Maleszewski <numerant@gmail.com>
2014-12-10 10:09:17 +0000

    [Model] Lowered AI complexity level (for better performance)

--- jMadNumbers/src/model/AI.java
+++ jMadNumbers/src/model/AI.java
@@ -23,7 +23,7 @@ public class AI
 		
     	for (Point move : state.listMoves())
     	{
-    		Integer currentValue = MinValue(state.makeMove(move), 25, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    		Integer currentValue = MinValue(state.makeMove(move), 8, Integer.MIN_VALUE, Integer.MAX_VALUE);
     		if (currentValue > maxValue)
     		{
     			maxValue = currentValue;
